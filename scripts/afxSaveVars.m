e4cb60422e8dff56678172cf39fa5c74b421f8b1
afx1337 <101255001+afx1337@users.noreply.github.com>
2022-05-22 20:00:12 +0200

    init

---
+++ scripts/afxSaveVars.m
@@ -0,0 +1,16 @@
+function afxSaveVars(fname,names,vals)
+    fileID = fopen([fname '.txt'],'w');
+    for i = 1:length(names)
+        val = vals{i};
+        info(i).name = names{i};
+        info(i).value = val;
+        if isnumeric(val), val = num2str(val); end
+        if strcmp(val,'')
+            fprintf('\n')
+        else
+            fprintf(fileID,'% 20s:    %s\n',names{i},val);
+        end
+    end
+    fclose(fileID);
+    save([fname '.mat'],'info');
+end
