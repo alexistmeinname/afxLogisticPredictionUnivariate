e53ffd4b42dd3282bffc6958975afee8742fe2ab
afx1337 <max.wawrzyniak@medizin.uni-leipzig.de>
2022-05-23 18:09:55 +0200

    fix radiomics models

--- Radiomics_minimal.m
+++ Radiomics_minimal.m
@@ -4,17 +4,17 @@ addpath('scripts');
 FWHM = [5 9 13];
 
 [~,space.XYZmm,space.dim,space.mat] = afxVolumeLoad('masks\space2mm_small.nii');
-load('data\Radiomics_Training_Leipzig\input\demographics\design.mat');
-
-design.analysisName = 'minimal_model';
-design.FWHM = NaN;          % spatial smoothing 5,9,13
-design.nFold = 5;           % number of folds in k-fold cross validation
-design.fold = NaN;          % dummy var
-design.minPerfusion = 10;   % minimum perfusion maps per parameter
-design.minLesion = .05;     % minimum lesion coverage
 
 for i = 1:length(FWHM)
     s = tic;
+    % design
+    load('data\Radiomics_Training_Leipzig\input\demographics\design.mat');
+    design.analysisName = 'minimal_model';
+    design.FWHM = FWHM(i);      % spatial smoothing 5,9,13
+    design.nFold = 5;           % number of folds in k-fold cross validation
+    design.fold = NaN;          % dummy var
+    design.minPerfusion = 10;   % minimum perfusion maps per parameter
+    design.minLesion = .05;     % minimum lesion coverage
     % daten laden
     [x,y,masks] = afxPrepareDesign(design,space);
     % intaraktionen
