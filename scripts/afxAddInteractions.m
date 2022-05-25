52c29196f721124af64d48b29f3e97b8f383dc1b
afx1337 <max.wawrzyniak@medizin.uni-leipzig.de>
2022-05-25 11:46:20 +0200

    comments

--- scripts/afxAddInteractions.m
+++ scripts/afxAddInteractions.m
@@ -1,16 +1,19 @@
 function [x,design] = afxAddInteractions(x,design)
-    
-    for iInteracton = 1:length(design.interactions)
-        % https://stats.stackexchange.com/questions/11645/coding-an-interaction-between-a-nominal-and-a-continuous-predictor-for-logistic
+
+    % https://stats.stackexchange.com/questions/11645/coding-an-interaction-between-a-nominal-and-a-continuous-predictor-for-logistic
+     for iInteracton = 1:length(design.interactions)
+        % find all predictors
         for iPredictor = 1:length(design.interactions(iInteracton).val)
             idxInteraction(iPredictor) = find(strcmpi(design.predictors,design.interactions(iInteracton).val{iPredictor}),1);
             if isempty(idxInteraction(iPredictor))
                 error(['Interaktion mit unbekanntem Praediktor "' design.interactions(iInteracton).val{iPredictor} '".']);
             end
         end
+        % calculate interaction
         x(:,end+1,:) = prod(x(:,idxInteraction,:),2);
+        % save name of interaction
         design.predictors(end+1) = join(design.interactions(iInteracton).val,' x ');
-
+        % updata xRaw
         for iPatient = 1:length(design.patients)
             design.patients(iPatient).xRaw{end+1} = 'interaction';
         end
--- scripts/afxCBFRel.m
+++ scripts/afxCBFRel.m
@@ -1,18 +1,25 @@
 function CBVRel = afxCBFRel(x,y,design,space,masks)
-    idxCBV = find(strcmpi(design.predictors,'CBF'),1);
+
+    % find CBF
+    idxCBF = find(strcmpi(design.predictors,'CBF'),1);
     CBVRel = nan(size(x,1),1,size(x,3));
     for iPatient = 1:size(x,1)
-        tmpCBV = reshape(afxDeMask(masks.analysis,x(iPatient,idxCBV,:),NaN),space.dim);
+        % get 3d CBF and 3d lesion
+        tmpCBF = reshape(afxDeMask(masks.analysis,x(iPatient,idxCBF,:),NaN),space.dim);
         tmpLesion = reshape(afxDeMask(masks.analysis,y(iPatient,:),NaN),space.dim);
+        % calculate lesion in lh and rh
         lh = tmpLesion(1:floor(end/2),:,:); lh = nansum(lh(:));
         rh = tmpLesion(ceil(end/2):end,:,:); rh = nansum(rh(:));
+        % get CBF from unaffected hemisphere
         if lh > rh
-            tmpCBVHemi = (tmpCBV(ceil(end/2):end,:,:));
+            tmpCBVHemi = (tmpCBF(ceil(end/2):end,:,:));
         else
-            tmpCBVHemi = (tmpCBV(1:floor(end/2),:,:));
+            tmpCBVHemi = (tmpCBF(1:floor(end/2),:,:));
         end
+        % mean CBF
         tmpCBVHemiMean = nanmean(tmpCBVHemi(:));
-        tmpCBVRel = tmpCBV(:)./tmpCBVHemiMean;
+        % CBFrel
+        tmpCBVRel = tmpCBF(:)./tmpCBVHemiMean;
         CBVRel(iPatient,1,:) = tmpCBVRel(masks.analysis);
     end
 end
