65c51521cf0ade70fa1e9cb778856bf86459a24a
afx1337 <max.wawrzyniak@medizin.uni-leipzig.de>
2022-05-25 18:55:19 +0200

    drop fold dummy var

--- Radiomics_full.m
+++ Radiomics_full.m
@@ -12,7 +12,6 @@ for i = 1:length(FWHM)
     design.analysisName = 'full_model';
     design.FWHM = FWHM(i);      % spatial smoothing 5,9,13
     design.nFold = 5;           % number of folds in k-fold cross validation
-    design.fold = NaN;          % dummy var
     design.minPerfusion = 10;   % minimum perfusion maps per parameter
     design.minLesion = .05;     % minimum lesion coverage
     design.interactions(1).val = {'CBF' 'tici'};
@@ -20,7 +19,6 @@ for i = 1:length(FWHM)
     design.interactions(3).val = {'Tmax' 'tici'};
     % daten laden
     [x,y,masks,design] = afxPrepareDesign(design,space);
-    % intaraktionen
     % k-fold crossvalidation (fitting des glms, prediction, abspeichern aller ergebnisse)
     afxKFold(x,y,masks,space,design);
     design.analysisName = strcat(design.analysisName,'-one-fold');
