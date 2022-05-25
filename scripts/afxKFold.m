9ba537e52a2402285416666d6dc9406843931398
afx1337 <max.wawrzyniak@medizin.uni-leipzig.de>
2022-05-25 11:45:45 +0200

    seperate k-fold and prediction

--- scripts/afxKFold.m
+++ scripts/afxKFold.m
@@ -1,18 +1,13 @@
 function [stats,predictions,mRSquared,design] = afxKFold(x,y,masks,space,design)
     
     nPatients = size(x,1);
-    
     % shuffle design and data
     pIdx = randperm(nPatients);
-    design = afxPartialDesign(design,pIdx);
+    design.patients = design.patients(pIdx);
     x = x(pIdx,:,:);
     y = y(pIdx,:);
     % patients in testset per fold
     perFold = nPatients/design.nFold;
-    % copy design matrix before generating intaractions
-    xCopy = x;
-    [x,design] = afxAddInteractions(x,design);
-    
     % perform folds
     patientsNew = struct([]);
     for iFold = 1:design.nFold
@@ -21,46 +16,16 @@ function [stats,predictions,mRSquared,design] = afxKFold(x,y,masks,space,design)
         idxTest = false(1,nPatients);
         idxTest(round(perFold*(iFold-1)+1):round(iFold*perFold)) = true;
         idxTrain = ~idxTest;
+        % save fold in patient struct
         for i = find(idxTest), design.patients(i).fold = iFold; end
-        % fit and save GLM (training data)
-        [stats,scale] = afxLogisitcGLMfit(x(idxTrain,:,:),y(idxTrain,:));
-        mRSquared(iFold,:) = afxSaveModel(stats,masks,space,scale,afxPartialDesign(design,idxTrain));
-        % generate design matix for sucessfull and unsucessful reka and
-        % predict (it's complicated, becaus all interactions need to be
-        % recalculated in the design matrix)
-        idxTICI = find(strcmpi(design.predictors,'tici'),1);
-        xReka = xCopy(idxTest,:,:);
-        xReka(:,idxTICI,:) = 0;
-        [xReka,~] = afxAddInteractions(xReka,design);
-        predictions.reka0 = afxLogisticGLMval([stats.beta],xReka,scale);
-        xReka = xCopy(idxTest,:,:);
-        xReka(:,idxTICI,:) = 1;
-        [xReka,~] = afxAddInteractions(xReka,design);
-        predictions.reka1 = afxLogisticGLMval([stats.beta],xReka,scale);
-        clear xReka;
-        % calculate mismatch
-        predictions.mismatch = predictions.reka0-predictions.reka1;
-        % calculate threshold (min abs.vol.diff.)
-        yfit = afxLogisticGLMval([stats.beta],x(idxTrain,:,:),scale);
-        optThr = afxOptimalThreshold(yfit,y(idxTrain,:),.001,false);
-        % save predictions
-        patientsGLMTest = afxSavePredictions(predictions,y(idxTest,:),masks,space,afxPartialDesign(design,idxTest),optThr);
-        % threshold variant
-        thresholdMaps = afxThresholdModel(x,y,idxTrain,design,space,masks);
-        designTest = design; designTest.patients = patientsGLMTest;
-        for iThrMap = 1:length(thresholdMaps)
-            thresholdMaps(iThrMap).dat = thresholdMaps(iThrMap).dat(idxTest,:);
-        end
-        patientsNew = [patientsNew afxSaveThreshold(thresholdMaps,masks,space,designTest)];
+        % perform prediction
+        [patientsTest,mRSquared(iFold,:)] = afxPrediction(x,y,masks,space,design,idxTrain,idxTest);
+        patientsNew = [patientsNew patientsTest];
     end
     % save mean R squared (mean of all folds)
     destDir = fullfile(design.dataDir,'output',strcat(design.analysisName,'-s',num2str(design.FWHM)));
     afxWritePredictors(fullfile(destDir,'models','meanRSquared.txt'),design.predictors,mean(mRSquared,1));
-    % update design ()
+    % update and save design
     design.patients = patientsNew;
     save(fullfile(destDir,'design.mat'),'design');
-end
-
-function design = afxPartialDesign(design,idx)
-    design.patients = design.patients(idx);
 end
---
+++ scripts/afxPrediction.m
@@ -0,0 +1,35 @@
+function [patientsTest,mRSquared] = afxPrediction(x,y,masks,space,design,idxTrain,idxTest)
+    
+    % fit and save GLM (training data)
+    [stats,scale] = afxLogisitcGLMfit(x(idxTrain,:,:),y(idxTrain,:));
+    mRSquared = afxSaveModel(stats,masks,space,scale,afxPartialDesign(design,idxTrain));
+    % generate design matix for sucessfull and unsucessful reka and
+    % predict (it's complicated, becaus all interactions need to be
+    % recalculated in the design matrix)
+    idxTICI = find(strcmpi(design.predictors,'tici'),1);
+    xReka = x(idxTest,1:(length(design.predictors)-length(design.interactions)),:);
+    xReka(:,idxTICI,:) = 0;
+    [xReka,~] = afxAddInteractions(xReka,design);
+    predictions.reka0 = afxLogisticGLMval([stats.beta],xReka,scale);
+    xReka = x(idxTest,1:(length(design.predictors)-length(design.interactions)),:);
+    xReka(:,idxTICI,:) = 1;
+    [xReka,~] = afxAddInteractions(xReka,design);
+    predictions.reka1 = afxLogisticGLMval([stats.beta],xReka,scale);
+    clear xReka;
+    % calculate threshold (min abs.vol.diff.)
+    yfit = afxLogisticGLMval([stats.beta],x(idxTrain,:,:),scale);
+    optThr = afxOptimalThreshold(yfit,y(idxTrain,:),.001,false);
+    % save predictions
+    patientsTest = afxSavePredictions(predictions,y(idxTest,:),masks,space,afxPartialDesign(design,idxTest),optThr);
+    % threshold variant
+    thresholdMaps = afxThresholdModel(x,y,idxTrain,design,space,masks);
+    designTest = design; designTest.patients = patientsTest;
+    for iThrMap = 1:length(thresholdMaps)
+        thresholdMaps(iThrMap).dat = thresholdMaps(iThrMap).dat(idxTest,:);
+    end
+    patientsTest = afxSaveThreshold(thresholdMaps,masks,space,designTest);
+end
+
+function design = afxPartialDesign(design,idx)
+    design.patients = design.patients(idx);
+end
