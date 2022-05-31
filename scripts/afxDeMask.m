38a8c086f8a914ba89c2ee22fec62e8a417d66ba
afx1337 <max.wawrzyniak@medizin.uni-leipzig.de>
2022-05-31 10:03:50 +0200

    minor changes

--- scripts/afxDeMask.m
+++ scripts/afxDeMask.m
@@ -1,5 +1,5 @@
 function img = afxDeMask(mask,dat,val)
-    img = zeros(size(dat,1),size(mask,2));
+    img = nan(size(dat,1),size(mask,2));
     if exist('val','var')
         img(:) = val;
     end
