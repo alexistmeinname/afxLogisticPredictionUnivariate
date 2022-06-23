f99bd0764521504e8a8e4318afa26d3ef0cc778a
afx1337 <max.wawrzyniak@medizin.uni-leipzig.de>
2022-06-23 11:23:44 +0200

    set Dice to Nan when groundtruth is missing

--- scripts/afxEvalDice.m
+++ scripts/afxEvalDice.m
@@ -3,6 +3,10 @@ function dice = afxEvalDice(a,b)
     dice = nan(1,size(a,1));
     parfor iPatient = 1:size(a,1)
         % https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
-        dice(iPatient) = 2*nnz(a(iPatient,:)&b(iPatient,:))/(nnz(a(iPatient,:))+nnz(b(iPatient,:)));
+        if nnz(b(iPatient,:)) == 0
+            dice(iPatient) = NaN;
+        else
+            dice(iPatient) = 2*nnz(a(iPatient,:)&b(iPatient,:))/(nnz(a(iPatient,:))+nnz(b(iPatient,:)));
+        end
     end
 end
