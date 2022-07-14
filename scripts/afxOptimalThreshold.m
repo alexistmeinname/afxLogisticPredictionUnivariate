aae65eeceed1b7314d830ad951dc46713307955b
afx1337 <max.wawrzyniak@medizin.uni-leipzig.de>
2022-07-14 10:55:19 +0200

    added comments

--- scripts/afxOptimalThreshold.m
+++ scripts/afxOptimalThreshold.m
@@ -1,26 +1,33 @@
 function [optThr,absDiff] = afxOptimalThreshold(yfit,y,tol,inverse)
-    sumVol = sum(y,2);
+    % optimal threhold minimizes sum/mean of absVolDiff
+    % this script uses an adaptive/iterative grid search for efficiency
+    
+    % for debugging purposes
+    %load('D:\projects\afxRs\afxLogisticPrediction\tmp_optThr.mat');
+    
+    gridSize = 10;
+    sumVolGT = sum(y,2);
     boundaries = [ min(yfit(:))  max(yfit(:))];
-    while abs(diff(boundaries)) > tol
-        boundaries = afxFindMinGrid(yfit,sumVol,inverse,boundaries,10);
+    while abs(diff(boundaries)) > 2*tol
+        boundaries = afxFindMinGrid(yfit,sumVolGT,inverse,boundaries,gridSize);
     end
     optThr = round(mean(boundaries),floor(-log10(tol)));
-    absDiff = afxAbsDiff(yfit,sumVol,optThr,inverse);
+    absDiff = afxAbsDiff(yfit,sumVolGT,optThr,inverse);
 end
     
-function absDiff = afxAbsDiff(yfit,sumVol,threshold,inverse)
+function absDiff = afxAbsDiff(yfit,sumVolGT,threshold,inverse)
     if inverse
-        absDiff = sum(abs(sumVol-sum(yfit<threshold,2)));
+        absDiff = sum(abs(sumVolGT-sum(yfit<threshold,2)));
     else
-        absDiff = sum(abs(sumVol-sum(yfit>threshold,2)));
+        absDiff = sum(abs(sumVolGT-sum(yfit>threshold,2)));
     end
 end
 
-function boundaries = afxFindMinGrid(yfit,sumVol,inverse,boundaries,n)
+function boundaries = afxFindMinGrid(yfit,sumVolGT,inverse,boundaries,n)
     thrs = boundaries(1):(boundaries(2)-boundaries(1))/(n-1):boundaries(2);
     absDiffs = nan(1,length(thrs));
     for i = 1:length(thrs)
-        absDiffs(i) = afxAbsDiff(yfit,sumVol,thrs(i),inverse);
+        absDiffs(i) = afxAbsDiff(yfit,sumVolGT,thrs(i),inverse);
     end
     %plot(thrs,absDiffs);
     [~,idx] = min(absDiffs);
