6c339ed7970d4e1dbacceba0c02c2b4e9022bfa4
alexistmeinname <74628004+alexistmeinname@users.noreply.github.com>
2023-10-26 13:37:54 +0200

    fixed reduction iteration

--- Radiomics_Train_all.m
+++ Radiomics_Train_all.m
@@ -16,17 +16,17 @@ reduce(5).val = 'tToImg';
 reduce(6).type = 'interaction';
 reduce(6).val = 3; %CBF x gmTPM index after reducing Tmax x tici (line 63-65)
 reduce(7).type ='interaction';
-reduce(7).val = 4; %Tmax x gTPM
+reduce(7).val = 4; %Tmax x gmTPM
 reduce(8).type = 'interaction';
-reduce(8).val = 3; %CBV x gTPM
-reduce(9).type = 'factor';
+reduce(8).val = 3; %CBV x gmTPM
+reduce(9).type = 'factor';CBV
 reduce(9).val = 'gmTPM'; % actually very small mean r^2, but can't be deleted before interactions
 reduce(10).type = 'factor';
 reduce(10).val = 'CT-N';
 reduce(11).type = 'interaction';
 reduce(11).val = 1; % CBF x tici
 reduce(12).type = 'interaction';
-reduce(12).val = 2; % CBV x tici
+reduce(12).val = 1; % CBV x tici
 reduce(13).type = 'factor';
 reduce(13).val = 'nihss';
 reduce(14).type = 'factor';
@@ -37,13 +37,13 @@ FWHM = [9 13 5 0];
 [~,space.XYZmm,space.dim,space.mat] = afxVolumeLoad('masks\space2mm_small.nii');
 
 
-for iReduce = 1:length(reduce)
+for iReduce = 12:length(reduce)
     for iFWHM = 1:length(FWHM)
         s = tic;
         % get design 
         load('data\Radiomics_Training_Leipzig\input\demographics\design.mat'); 
         brainMask = fullfile('masks','brainmask.nii');
-        %design.patients = design.patients(1:20);
+        %design.patients = design.patients(10:20);
         if iReduce == 0
             design.analysisName = 'full_model';
         else
