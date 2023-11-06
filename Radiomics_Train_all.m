644505c37fef8bffc3fb39593d69ec0e824bf5eb
alexistmeinname <74628004+alexistmeinname@users.noreply.github.com>
2023-11-06 13:01:41 +0100

    Undersampling redone
    undersampling now chooses same amount of voxel with lesion and without lesion from each individual patient

--- Radiomics_Train_all.m
+++ Radiomics_Train_all.m
@@ -16,9 +16,15 @@ reduce(6).val = 'CT-N';
 reduce(7).type = 'factor';
 reduce(7).val = 'nihss';
 reduce(8).type = 'interaction';
+<<<<<<< Updated upstream
 reduce(8).val = 1; % CBF x tici
 reduce(9).type = 'interaction';
 reduce(9).val = 1; % CBV x tici
+=======
+reduce(8).val = 3; %CBV x gmTPM
+reduce(9).type = 'factor';
+reduce(9).val = 'gmTPM'; % actually very small mean r^2, but can't be deleted before interactions
+>>>>>>> Stashed changes
 reduce(10).type = 'factor';
 reduce(10).val = 'CBV';
 
@@ -26,7 +32,12 @@ FWHM = [9 13 5 0];
 
 [~,space.XYZmm,space.dim,space.mat] = afxVolumeLoad('masks\space2mm_small.nii');
 
+<<<<<<< Updated upstream
 for iReduce = 0:length(reduce)
+=======
+
+for iReduce = 0:0%length(reduce)
+>>>>>>> Stashed changes
     for iFWHM = 1:length(FWHM)
         s = tic;
         % design
@@ -54,8 +65,14 @@ for iReduce = 0:length(reduce)
             end
         end
         % daten laden
+<<<<<<< Updated upstream
         [x,y,masks,design] = afxPrepareDesign(design,space);
         % k-fold crossvalidation (fitting des glms, prediction, abspeichern aller ergebnisse)
+=======
+        [x,y,masks,design] = afxPrepareDesign(design,space,brainMask);
+        
+        % k-fold crossvalidation 
+>>>>>>> Stashed changes
         designFile = afxKFold(x,y,masks,space,design);
         % evaluation
         afxEvaluatePredictions(designFile);
--- scripts/afxLogisitcGLMfit.m
+++ scripts/afxLogisitcGLMfit.m
@@ -10,13 +10,65 @@ function [stats,scale] = afxLogisitcGLMfit(x,y)
     % stats.sse  ... sum of squared errors
     % scale      ... scale factors for x (scale.mean, scale.std)
     %
+<<<<<<< Updated upstream
     % NaNs in x or y are treated as missing values
 
     s = tic;
+=======
+    %NaNs in x or y are treated as missing values
+    s = tic;
+    % fit logistic glm for every voxel
+    fprintf('Fitting logistic GLMs [');
+    %ws = warning('off');
+    y = cast(y,"single");
+    x = cast(x,"single");
+    
+    
+    
+    % random stratified undersampling
+  
+    matrixOverweigt = y==round(mean(y));  %patients x voxel, 1== keine Läsion, 0 == Läsion
+    % round geht pro patient immer auf 0, weil mehr voxel ohne läsion
+    % vorhanden; dadurch invertierung der matrix (alle mit 0 werden 1 und
+    % andersherum
+    
+    %mark all values that are nan in x 
+    nanMatrixOverweigt =  squeeze(any(isnan(x),2));
+    
+    for patient = 1: size(matrixOverweigt,1)
+        nDel = nnz(matrixOverweigt(patient,:))-nnz(~matrixOverweigt(patient,:))-nnz(nanMatrixOverweigt(patient,:));
+        indexOverweigt = find(matrixOverweigt(patient,:)); %indices aller voxel mit 1 (inklusive Nans)
+        indexnanOverweigt = find(nanMatrixOverweigt(patient,:));%indices aller NaN-voxel
+        indexOverweigt = setdiff(indexOverweigt,indexnanOverweigt); %delete all voxel with nans from indesOverweigt
+        indexOverweigt = indexOverweigt(randperm(nnz(indexOverweigt)));
+        if nDel <= length(indexOverweigt)
+            idxDel = indexOverweigt(1:nDel);
+        else
+            nDel = length(indexOverweigt);
+            idxDel = indexOverweigt(1:nDel);
+        end
+        x(patient,:,idxDel) = NaN;
+    end
+   
+    clearvars -except x y s;
+    % reshape input data (x, y) and clear x,y (for freeing memory)
+    [x,y] = afxReshapeData(x,y);
+    
+    % delete observations with NaNs (were either NaN from the beginning or
+    % to be deleted due to Random Stratified Undersampling
+    xnan = any(isnan(x),2);
+    x(xnan,:) = [];
+    y(xnan) = [];
+    
+    x = cast(x, "double")
+    y = cast(y,"double");
+    
+>>>>>>> Stashed changes
     % scale input data
     scale.mean = nanmean(x,1);
     scale.std = nanstd(x,1);
     x = (x-scale.mean)./scale.std;
+<<<<<<< Updated upstream
     % fit logistic glm for every voxel
     stats = struct([]);
     pct = size(x,3)/50;
@@ -30,5 +82,18 @@ function [stats,scale] = afxLogisitcGLMfit(x,y)
         if mod(iVoxel,pct) < 1, fprintf('.'); end
     end
     warning(ws);
+=======
+   
+    
+    % fit GLM
+    [b,~,statistics] = glmfit(x,y,'binomial','link','logit');
+    stats = struct('t',{statistics.t},'beta',{b},'dfe',{statistics.dfe},'mse',{nanmean(statistics.resid.^2)});
+
+    %warning(ws);
+>>>>>>> Stashed changes
     fprintf('] (%.2f min)\n',toc(s)/60);
+    
+    
+    
+ 
 end
